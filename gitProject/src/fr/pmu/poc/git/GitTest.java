de6b1a7911368d58cdfa690416e44f78ae3c7c14
P093839 <sebastien_le-sausse@pmu.fr>
2011-08-31 16:00:07 +0200

    Modification de la classe Gittest
    Signed-off-by: P093839 <sebastien_le-sausse@pmu.fr>

--- gitProject/src/fr/pmu/poc/git/GitTest.java
+++ gitProject/src/fr/pmu/poc/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git is fun !");
+		System.out.println("This classe is on GitHub remote");
 	}
 
 }
