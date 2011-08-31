7e93279ae704e7defac52d5d1cee26be3ebd4731
P093839 <sebastien_le-sausse@pmu.fr>
2011-08-31 14:47:28 +0200

    Creation de Hello world
    Signed-off-by: P093839 <sebastien_le-sausse@pmu.fr>

---
+++ gitProject/src/fr/pmu/poc/git/Hello.java
@@ -0,0 +1,13 @@
+package fr.pmu.poc.git;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world !!!");
+	}
+
+}
