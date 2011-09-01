a60eb051ab7a45c7c7bdd08b09b70087f984b46b
P093839 <sebastien_le-sausse@pmu.fr>
2011-09-01 11:43:26 +0200

    Suppression de classe dans le projet
    Signed-off-by: P093839 <sebastien_le-sausse@pmu.fr>

--- gitProject/src/fr/pmu/poc/git/GitTest.java
+++ gitProject/src/fr/pmu/poc/git/GitTest.java
@@ -8,7 +8,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git is fun !");
-		System.out.println("Plus rien");
 	}
 
 }
