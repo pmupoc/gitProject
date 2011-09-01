e51fcac367cc74b0bf48b5965bbab831721642bc
P093839 <sebastien_le-sausse@pmu.fr>
2011-09-01 11:35:03 +0200

    Création d'une nouvelle branche en vue d'un merge
    Signed-off-by: P093839 <sebastien_le-sausse@pmu.fr>

---
+++ gitProject/src/fr/pmu/poc/git/BeanCreateInBranch.java
@@ -0,0 +1,13 @@
+package fr.pmu.poc.git;
+
+public class BeanCreateInBranch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("J'ai été créé dans une branche");
+	}
+
+}
